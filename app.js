e2e260a38f47fc2de37ad0bcae327170282496b7
r48patel <r48patel@gmail.com>
2017-03-05 16:09:21 -0600

    Blog template done

--- app.js
+++ app.js
@@ -12,13 +12,42 @@ app.set('views', path.join(__dirname , 'views'));
 app.set('public', path.join(__dirname , 'public'));
 app.set('view engine', 'ejs');
 
+
+
+function get_blogs_info(){
+	blogs_list=[]
+	file = path.join('views', 'pages', 'blog_list');
+	file_info = fs.readFileSync(file).toString().split('\n')
+	var blog_info = {}
+	file_info.forEach(line => {
+		if (line == '-') {
+			blogs_list.push(blog_info)
+			blog_info = {}
+		}
+		else {
+			line_split = line.split(':')
+			blog_info[line_split[0].trim()] = line_split[1].trim()
+		}
+	})
+	return blogs_list
+}
+
 app.get('/', function(request, response) {
   response.redirect('/index');
 });
 
 app.get('/:type', function(request, response) {
     reqType = request.params.type
-    response.render(path.join('pages', reqType), {});
+    if (!reqType.startsWith('favicon')){
+	    if (reqType == 'blog') {
+	    	response.render(path.join('pages', reqType), {
+	    		blogs_list: get_blogs_info()
+	    	});
+	    }
+	    else {
+	    	response.render(path.join('pages', reqType), {});
+		}
+	}
 });
 
 app.listen(app.get('port'), function() {
